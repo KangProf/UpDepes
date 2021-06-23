665e29869061b5cea29a421872d4e97cdb1b534e
Prof-acc <77473259+KangProf@users.noreply.github.com>
2021-06-23 18:10:41 +0700

    Update Depes.py

--- Depes.py
+++ Depes.py
@@ -61,7 +61,7 @@ def main(__bn__):
    print(__bn__)
    while True:
       try:
-         a = x("Enter your script deface name: ")
+         a = x("Masukan nama script deface mu => ")
          if not os.path.isfile(a):
             print("file '%s' not found"%(a))
             continue
